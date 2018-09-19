08f75b951e9b69a19802c820630cfc62136a758e
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2018-09-19 23:40:20 +0530

    Update code.js

---
+++ The Core/Intro Gates/Add Two Digits/code.js
@@ -0,0 +1,9 @@
+function addTwoDigits(n) {
+    n = String(n)
+    let result = 0;
+     for(let i = 0; i < n.length; i++) {
+          result += Number(n[i]);
+       } 
+    return result; 
+   }
+   
