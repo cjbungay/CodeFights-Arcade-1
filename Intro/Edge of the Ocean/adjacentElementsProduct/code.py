443e27ea74e89970dfb39ef0a25381709617f4e0
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2018-09-21 11:30:55 +0530

    Update code.py

--- Intro/Edge of the Ocean/adjacentElementsProduct/code.py
+++ Intro/Edge of the Ocean/adjacentElementsProduct/code.py
@@ -2,4 +2,11 @@ def adjacentElementsProduct(a):
   b=[]
   for i in range(len(a)-1):
     b.append(a[i]*a[i+1])
-  return max(b) 
+  return max(b)
+
+def adjacentElementsProduct(inputArray):
+  result = -float('inf')
+  for i in range(len(inputArray) - 1):
+      if inputArray[i] * inputArray[i+1] > result:
+          result = inputArray[i] * inputArray[i+1]
+  return result
